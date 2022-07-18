19d926b4b465e732f62dd2a0a86928fa56515274
Stephen Herr <stephenherr@microsoft.com>
2022-07-18 16:07:31 +0000

    Merged PR 6344973: Migrate to internal base image for security policy
    Microsoft's supply-chain security policy states that we must only pull from ACR/MCR base images, and this is currently causing every build to fail. Let's migrate to an MCR image, and also upgrade to python 3.10 why not.
    
    Related work items: #14815732

--- server/app/core/schemas.py
+++ server/app/core/schemas.py
@@ -363,7 +363,9 @@ class AccountCreate(BaseModel):
 
 class AccountUpdate(BaseModel):
     # reuse AccountCreate but make fields optional
-    __annotations__ = {k: Optional[v] for k, v in AccountCreate.__annotations__.items()}  # type: ignore #
+    __annotations__ = {}
+    for k, v in AccountCreate.__annotations__.items():
+        __annotations__[k] = Optional[v]  # type: ignore
 
 
 class AccountResponse(AccountCreate):
