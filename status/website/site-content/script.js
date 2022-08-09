cdfb544efc9221890f485e3eeb49ace76dc3a1e5
Adrian Abedon <t-adabedon@microsoft.com>
2022-08-09 18:07:47 +0000

    Merged PR 6564207: Changes status site layout, added last checked time for apt dists
    - status site contains last checked time for apt dists
    - changed errors button to a dropdown error
    - changed layout of list of apt dists in a repository
    
    Checkout the latest changes here https://pmcstatusprimary.z5.web.core.windows.net/apt.html

--- status/website/site-content/script.js
+++ status/website/site-content/script.js
@@ -1,5 +1,61 @@
 STATUS_URL = "https://pmcstatusprimary.blob.core.windows.net/dynamic-public-data/repository_status.json"
 
+
+/**
+ * Returns plural form of word if count is more than 1,
+ * otherwise returns singular
+ * 
+ * @param {int} count 
+ * @param {string} singular 
+ * @param {string} plural 
+ * @returns 
+ */
+function pluralize(count, singular, plural) {
+  if (count == 1) {
+      return singular
+  } else {
+      return plural
+  }
+}
+
+
+/**
+ * Get an elapsed time string. The update time is expected
+ * to be formatted as a UTC string.
+ * 
+ * @param {string} update_time_str 
+ * @returns Elapsed time as a readable string.
+ */
+function get_elapsed_time_string(update_time_str) {
+  _MS_PER_SECOND = 1000
+  _MS_PER_MINUTE = _MS_PER_SECOND * 60
+  _MS_PER_HOUR = _MS_PER_MINUTE * 60
+  _MS_PER_DAY = _MS_PER_HOUR * 24
+
+  update_time_str = update_time_str.substring(0, update_time_str.length - 7)
+  update_time_str = update_time_str.replace(/-/g, '/')
+  const time_now = new Date()
+  const update_time = new Date(update_time_str + " UTC")
+
+  milli_diff = time_now - update_time
+
+  day_diff = Math.floor(milli_diff / _MS_PER_DAY)
+  hour_diff = Math.floor(milli_diff / _MS_PER_HOUR)
+  minute_diff = Math.floor(milli_diff / _MS_PER_MINUTE)
+  second_diff = Math.floor(milli_diff / _MS_PER_SECOND)
+
+  if (day_diff > 0) {
+    return `${day_diff} ${pluralize(day_diff, "day", "days")} ago`
+  } else if (hour_diff > 0) {
+    return `${hour_diff} ${pluralize(hour_diff, "hour", "hours")} ago`
+  } else if (minute_diff > 0) {
+    return `${minute_diff} ${pluralize(minute_diff, "minute", "minutes")} ago`
+  } else {
+    return `${second_diff} ${pluralize(second_diff, "second", "seconds")} ago`
+  }
+}
+
+
 /**
  * Get mirror status from STATUS_URL and generate HTML content for the page.
  */
@@ -54,6 +110,7 @@ function get_mirror() {
   });
 }
 
+
 /**
  * Returns true if the checkbox is checked, false otherwise.
  * 
@@ -65,41 +122,6 @@ function is_checked(checkbox_id) {
   return elem == null || elem.checked
 }
 
-/**
- * Get an elapsed time string. The update time is expected
- * to be formatted as a UTC string.
- * 
- * @param {string} update_time_str 
- * @returns Elapsed time as a readable string.
- */
-function get_elapsed_time_string(update_time_str) {
-  _MS_PER_SECOND = 1000
-  _MS_PER_MINUTE = _MS_PER_SECOND * 60
-  _MS_PER_HOUR = _MS_PER_MINUTE * 60
-  _MS_PER_DAY = _MS_PER_HOUR * 24
-
-  update_time_str = update_time_str.substring(0, update_time_str.length - 7)
-  update_time_str = update_time_str.replace(/-/g, '/')
-  const time_now = new Date()
-  const update_time = new Date(update_time_str + " UTC")
-
-  milli_diff = time_now - update_time
-
-  day_diff = Math.floor(milli_diff / _MS_PER_DAY)
-  hour_diff = Math.floor(milli_diff / _MS_PER_HOUR)
-  minute_diff = Math.floor(milli_diff / _MS_PER_MINUTE)
-  second_diff = Math.floor(milli_diff / _MS_PER_SECOND)
-
-  if (day_diff > 0) {
-    return `${day_diff} day${day_diff == 1 ? "" : "s"} ago`
-  } else if (hour_diff > 0) {
-    return `${hour_diff} hour${hour_diff == 1 ? "" : "s"} ago`
-  } else if (minute_diff > 0) {
-    return `${minute_diff} minute${minute_diff == 1 ? "" : "s"} ago`
-  } else {
-    return `${second_diff} second${second_diff == 1 ? "" : "s"} ago`
-  }
-}
 
 /**
  * Get repository status from STATUS_URL and generate HTML content for the page.
@@ -118,7 +140,7 @@ function get_repo(type) {
     }
     apt_status = status[type];
 
-    accordion_content = ``
+    accordion_content = ""
     $.each(apt_status, function (repo, repo_data) {
       empty = false
       if (repo_data["state"] == "empty") {
@@ -142,16 +164,18 @@ function get_repo(type) {
         svg = '<img src = "svg-error.svg" width="18px" height="18px" alt="Fail"/>'
       }
 
+      last_updated = ""
       if ("time" in repo_data) {
         time_str = get_elapsed_time_string(repo_data["time"])
-      } else {
-        time_str = ""
+        last_updated = `<span style="color: grey">Checked ${time_str}</span>`
       }
-      last_updated = `<span style="color: grey">Checked ${time_str}</span>`
 
-      accordion_body_content = ``
+      accordion_body_items = ""
       if (empty) {
-        accordion_body_content += "This repository is empty"
+        accordion_body_items += `
+        <li class="list-group-item">
+          This repository is empty
+        </li>`
       } else {
         $.each(repo_data["dists"], function (dist, dist_data) {
           if (dist_data["state"] == "ok") {
@@ -160,11 +184,7 @@ function get_repo(type) {
             error_desc = ""
           } else {
             err_count = dist_data["dist_errors"].length
-            if (err_count == 1) {
-              error_str = "1 error found: "
-            } else {
-              error_str = `${err_count} errors found:`
-            }
+            error_str = `${err_count} ${pluralize(err_count, "error", "errors")} found:`
             dist_color = "red"
             error_desc = "<ul>"
             for (var error of dist_data["dist_errors"]) {
@@ -173,37 +193,54 @@ function get_repo(type) {
             error_desc += "</ul>"
           }
 
+          dist_last_updated = ""
           if (dist == "yum_dist" || dist == "apt_dist") {
-            title = `${error_str}`
+            title = `<span style="color:${dist_color}"> ${error_str} </span>`
           } else {
-            title = `>> ${dist}: ${error_str}`
+            title = `<b>${dist}:</b> <span style="color:${dist_color}"> ${error_str} </span>`
+            if ("time" in dist_data) {
+              time_str = get_elapsed_time_string(dist_data["time"])
+              dist_last_updated = `<span style="color: grey"> - Checked ${time_str}</span>`
+            }
           }
-          dist_errors_button = ``
-          dist_errors_content = ``
+          dist_errors_button = ""
+          dist_errors_content = ""
 
           if (error_desc.length > 0) {
             dist_errors_button = `
-              <button class="btn btn-secondary btn-sm" type="button"
+              <img type="button"
+                id="dropdown${idx}"
+                onClick="dropdown_click(event)"
                 data-bs-toggle="collapse" 
                 data-bs-target="#errors-div${idx}" aria-expanded="false" 
-                aria-controls="errors-div${idx}">
-                Errors
-              </button>`
+                aria-controls="errors-div${idx}" 
+                src = "svg-down.svg" width="24px" height="24px" alt="Fail"/>
+              `
             dist_errors_content = `
               <div class="collapse" id="errors-div${idx}">
-                <div class="card card-body">
-                  ${error_desc}
-                </div>
+                <p>
+                  <div class="card card-body">
+                    ${error_desc}
+                  </div>
+                </p>
               </div>`
             idx += 1
           }
-          accordion_body_content += `
-          <p style="color:${dist_color}">
-            ${title} ${dist_errors_button}
-          </p>
-          ${dist_errors_content}`
+
+          accordion_body_items += `
+          <li class="list-group-item">
+              ${title} ${dist_last_updated} ${dist_errors_button}
+
+            ${dist_errors_content}
+          </li>`
         });
       }
+
+      accordion_body_content = `
+        <ul class="list-group list-group-flush">
+          ${accordion_body_items}
+        </ul>`
+
       button_text = `
         <div class="container">
           <div class="row">
@@ -240,6 +277,23 @@ function get_repo(type) {
     document.getElementById("main-content").innerHTML = page
   });
 }
+
+
+/**
+ * onClick function for error dropdowns. Switches dropdown arrow between
+ * up and down arrow. 
+ * 
+ * @param {PointerEvent} event 
+ */
+function dropdown_click(event) {
+  console.log(event)
+  if (event.target.src.includes("svg-down.svg")) {
+    event.target.src = "svg-up.svg"
+  } else {
+    event.target.src = "svg-down.svg"
+  }
+}
+
 
 /**
  * Wrapper for get_mirror and get_repo. Useful for the reload button.
