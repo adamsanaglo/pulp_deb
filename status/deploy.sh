5b66a6cfae9081df7b500e57e5ae22b6f889f7d2
Adrian Abedon <t-adabedon@microsoft.com>
2022-08-16 16:08:08 +0000

    Merged PR 6586569: Deploy a testing status site
    - Deploy a testing status site along with the Azure function backend entirely using the Azure CLI
    - Configures all parts of the functions and storage through the CLI
    - If anything, let this be my guide on how to deploy this before I leave
    
    Related work items: #15276623

--- status/deploy.sh
+++ status/deploy.sh
@@ -3,31 +3,64 @@
 # the Azure Function code to run in Azure.
 
 resourceGroup=statusmonitor
+resourceGroupPremiumAppService=${resourceGroup}-premium
 location=centralus
 storageAccount=pmcstatus
 consumption=consumption # Low-cost Azure Function offering
 premium=premium         # Upper tier Azure Function offering
 appPlans=(${consumption} ${premium} ${consumption})
 dirNames=("pmc_scan_mirrors" "pmc_scan_repos" "pmc_status_delivery")
+skuStoragePrimary="Standard_RAGRS"
+skuStorageSecondary="Standard_LRS"
+functionsVersion="4"
 
 function bail() {
     >&2 echo "${@}"
     exit 1
 }
 
+function createResourceGroups() {
+    echo "Creating $resourceGroup in "$location"..."
+    az group create --name $resourceGroup --location "$location"
+
+    # Create resource group for the premium app service plan
+    echo "Creating $resourceGroupPremiumAppService in "$location"..."
+    az group create --name $resourceGroupPremiumAppService --location "$location"
+}
+
 function createPrimaryStorage() {
     # Create Primary Storage Account that will host our website
-    az storage account create -n ${storageAccount} -g ${resourceGroup} -l ${location} --sku Standard_LRS
+    az storage account create -n ${storageAccount} -g ${resourceGroup} -l ${location} --sku $skuStoragePrimary
+}
+
+function configurePrimaryStorage() {
+    # Configure the primary storage account that serves as the center piece of status monitoring
+    az storage cors add --connection-string $primarystorage_connectionstring --origins '*' --methods GET --allowed-headers '*' --exposed-headers 'Content-Length' --max-age 5 --services b
+    
+    echo "Creating storage container dynamic-public-data"
+    az storage container create --connection-string $primarystorage_connectionstring --name dynamic-public-data --public-access blob
+    az storage blob upload --connection-string $primarystorage_connectionstring --container-name dynamic-public-data --name repository_status.json --file files/repository_status.json
+
+    echo "Creating storage container static-data"
+    az storage container create --connection-string $primarystorage_connectionstring --name static-data
+    az storage blob upload --connection-string $primarystorage_connectionstring --container-name static-data --name mirrors.json --file files/mirrors.json
+    az storage blob upload --connection-string $primarystorage_connectionstring --container-name static-data --name pubkeys.json --file files/pubkeys.json
+
+    echo 'Creating storage container $web'
+    az storage container create --connection-string $primarystorage_connectionstring --name '$web'
+
     pushd website/ > /dev/null
     az storage blob upload-batch -s site-content/ -d '$web' --account-name ${storageAccount} --overwrite --auth-mode key
     popd > /dev/null
     az storage blob service-properties update --account-name ${storageAccount} --static-website --404-document 404.html --index-document mirror.html --auth-mode login
 }
 
-function createAppServicePlans() {
-    # Create app service plans that will host our Azure Functions
-    az appservice plan create -g ${resourceGroup} --is-linux -n ${consumption} -l ${location}
-    az appservice plan create -g ${resourceGroup} --is-linux -n ${premium} --sku P1v2 -l ${location}
+function createAppServicePlan() {
+    # Create premium app service plan, which will hold a single function (pmcscanrepos)
+    # Must exist in a separate resource group to separate it from consumption plan(s)
+    # Consumption plans are created automatically when functions are created
+    skuPlan="EP1"
+    az functionapp plan create --name $premium --resource-group $resourceGroupPremiumAppService --location "$location" --sku $skuPlan --max-burst 30 --min-instances 1 --is-linux true
 }
 
 function checkDirName() {
@@ -44,13 +77,29 @@ function createFunction() {
     dirName="${1}"
     checkDirName "${dirName}"
     shortName=$(echo "${dirName}" | tr -d '_')
-    appPlan=${2}
 
     # Create Storage account for this function
     az storage account create -n ${shortName} -g ${resourceGroup} -l ${location} --sku Standard_LRS
     
     # Create Azure Function within the specified App Service Plan
-    az functionapp create -s ${shortName} -g ${resourceGroup} -p ${appPlan} -n ${shortName} --os-type linux --runtime python
+    if [[ "${dirName}" == "${pmc_scan_repos}" ]]; then
+        premiumPlanId=$(az appservice plan show --name $premium --resource-group $resourceGroupPremiumAppService --query 'id' -o tsv)
+        planParam=" --plan $premiumPlanId"
+    else
+        planParam=" --consumption-plan-location $location"
+    fi
+    az functionapp create -s ${shortName} -g ${resourceGroup} -n ${shortName} ${planParam} --functions-version 4 --os-type linux --runtime python --runtime-version 3.9
+
+    # Configure storage account, which is present on all functions
+    az functionapp config appsettings set --name $shortName --resource-group $resourceGroup --settings "pmcstatusprimary_CONNECTION=$primarystorage_connectionstring"
+
+    # Configure settings specific to the "pmc_status_delivery" function
+    if [[ "${dirName}" == "pmc_status_delivery" ]]; then
+        az functionapp config appsettings set --name $functionApp --resource-group $resourceGroup --settings "JsonContainerName=dynamic-public-data"
+        az functionapp config appsettings set --name $functionApp --resource-group $resourceGroup --settings "JsonBlobName=repository_status.json"
+        az functionapp config appsettings set --name $functionApp --resource-group $resourceGroup --settings "ResultsQueueName=results-queue"
+        az resource update --resource-type Microsoft.Web/sites -g $resourceGroup -n $functionApp/config/web --set properties.functionAppScaleLimit=1
+    fi
 }
 
 function deployFunction() {
@@ -77,7 +126,10 @@ function deployFunctions() {
     done
 }
 
+createResourceGroups
 createPrimaryStorage
-createAppServicePlans
+primarystorage_connectionstring=$(az storage account show-connection-string --name $primarystorage --resource-group $resourceGroup --query "connectionString" -o tsv)
+configurePrimaryStorage
+createAppServicePlan
 createFunctions
 deployFunctions
