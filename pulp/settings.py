619eebc06888b3c8e7dbc2cce78102cb4e4076f1
David Davis <daviddavis@microsoft.com>
2022-09-14 16:54:55 +0000

    Merged PR 6744255: Disable telemetry in Pulp
    Disable telemetry in Pulp

--- pulp/settings.py
+++ pulp/settings.py
@@ -3,3 +3,4 @@ CONTENT_ORIGIN = "http://localhost:8080"
 ANSIBLE_API_HOSTNAME = "http://localhost:8080"
 ANSIBLE_CONTENT_HOSTNAME = "http://localhost:8080/pulp/content"
 TOKEN_AUTH_DISABLED = True
+TELEMETRY = False
