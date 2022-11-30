403bd39131e19781263715daa9407f74df21e25a
Jason Zions <jasonzio@microsoft.com>
2022-11-30 20:10:25 +0000

    Merged PR 7241671: Require explicit argument to activate new config
    Install script always drops the new vNext config, but by default will not activate it; an explicit argument is required to permit activation.
    
    Related work items: #16454817

--- edge/config-install.sh
+++ edge/config-install.sh
@@ -2,18 +2,32 @@
 # service roles.
 # This script assumes it, and the tarball containing it, were unpacked in the same directory.
 # It also assumes that the tarball was unpacked as root, and that the current user has sudo.
+#
+# The newly-installed configuration file is enabled if and only if the parameter "activate"
+# is passed to this script.
+#
+# Usage:   config-install.sh [activate]
+
+activate="$1"
 
 sudo install -o root -g root -m 555 -t /usr/local/bin config-activate.sh log-analyze
 
 available=/etc/nginx/sites-available
+enabled=/etc/nginx/sites-enabled
 
 # Save vCurrent configuration file so we can switch back to it.
 # Leave it in place, renamed; if nginx restarts, it will use it.
-sudo mv /etc/nginx/sites-enabled/ssl /etc/nginx/sites-enabled/vCurrent.conf
-sudo cp /etc/nginx/sites-enabled/vCurrent.conf $available
+if [ -f $enabled/ssl ]; then
+    sudo mv $enabled/ssl $enabled/vCurrent.conf
+fi
+if [ -f $enabled/vCurrent.conf]; then
+    sudo cp $enabled/vCurrent.conf $available
+fi
 
 # Install vNext configuration file.
 sudo cp sites-available/vNext.conf $available/vNext.conf
 
-# Activate the vNext configuration file.
-sudo /usr/local/bin/config-activate.sh vNext
+if [ "$activate" = "activate" ]; then
+    # Activate the vNext configuration file.
+    sudo /usr/local/bin/config-activate.sh vNext
+fi
