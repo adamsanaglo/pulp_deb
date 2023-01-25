eac02ab8b1b6a466a794f0e42cfda72a65fdfe7c
Stephen Herr <stephenherr@microsoft.com>
2023-01-25 16:39:25 +0000

    Merged PR 7521876: Add some sanity checks to the edge deploy script
    Try to download a couple of small rpms to ensure that our edge deployment went smoothly.

--- edge/config-activate.sh
+++ edge/config-activate.sh
@@ -13,3 +13,19 @@ else
     exit 1
 fi
 nginx -s reload
+
+# Sanity tests
+# 1) Should be able to download an "old path" rpm, validating ssl.
+curl --head --silent --output /tmp/myfile --resolve 'packages.microsoft.com:443:127.0.0.1' --resolve 'packages.microsoft.com:80:127.0.0.1' -L https://packages.microsoft.com/yumrepos/amlfs-el7/amlfs-lustre-client-2.15.1_24_gbaa21ca-3.10.0.1160.41.1.el7-1.noarch.rpm
+if [ "$?" = "0" ]; then
+  echo "Warning: downloading old-path rpm failed sanity check!"
+fi
+
+# 2) If we enabled vnext we should be able to download a "new path" rpm, validating ssl.
+if [ "$goal" = "vnext" ]; then
+  curl --head --silent --output /tmp/myfile --resolve 'packages.microsoft.com:443:127.0.0.1' --resolve 'packages.microsoft.com:80:127.0.0.1' -L https://packages.microsoft.com/yumrepos/amlfs-el7/Packages/a/amlfs-lustre-client-2.15.1_24_gbaa21ca-3.10.0.1160.41.1.el7-1.noarch.rpm
+  if [ "$?" = "0" ]; then
+    echo "Warning: downloading new-path rpm failed sanity check!"
+  fi
+fi
+rm /tmp/myfile
