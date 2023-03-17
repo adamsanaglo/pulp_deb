425c14458a4611fcdfdf8691095dbaee9cf4f05a
David Davis <daviddavis@microsoft.com>
2023-03-17 21:18:43 +0000

    Merged PR 7802029: Set rpm metadata generation setting in deployment config

--- pulp/settings.py
+++ pulp/settings.py
@@ -1,4 +1,7 @@
-# settings for pulp that get mounted into Pulp's containers
+# settings for pulp that get mounted into Pulp's development containers
+# any setting here will not affect deployments and will most likely also need to be set in
+# server/deployment/config.yml
+
 CONTENT_ORIGIN = "http://localhost:8081"
 PYPI_API_HOSTNAME = "http://localhost:8081"
 TOKEN_AUTH_DISABLED = True
